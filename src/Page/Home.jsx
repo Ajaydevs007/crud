b3471fb8f3fb22ce501945047dbc83249b81cfbb
Ajaydevs007 <ajaydevsv@gmail.com>
2025-02-18 11:24:10 +0530

    updated the background color

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -58,7 +58,7 @@ function Home() {
 
             <div className="container-fluid">
 
-                <div className='my-5' style={{backgroundColor:"black"}}>
+                <div className='my-5' style={{backgroundColor:"wheat"}}>
 
                     <Row>
 
